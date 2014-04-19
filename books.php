29dc476fef5e500ed5b25b7df2b3948f7068c59a
Jochen <jochen.hertle@arcor.de>
2014-04-19 10:20:39 +0200

    new version with automatic calibration and integration with zotero

--- books.php
+++ books.php
@@ -1,105 +1,114 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php
-// printing out php errors for debugging
-error_reporting (E_ALL | E_STRICT);
-ini_set ('display_errors' , 1);
-// includes
-include 'modules/m_books.php';
-$message = '';
+  // printing out php errors for debugging
+  error_reporting (E_ALL | E_STRICT);
+  ini_set ('display_errors' , 1);
+
+  include 'modules/m_books.php';
+  $message = '';
+  $row = '';
+  $dist = '';
 ?>
-
-<html>
-<head>
-<link rel="stylesheet" media="only screen and (max-device-width:480px)" href="css/style_handheld.css">
-<link rel="stylesheet" media="all" href="css/style_screen.css">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Books</title>
-</head>
-
-<body>
-
-<h2>Books</h2>
-<hr />
-<h3>Create books in the DB</h3>
-
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	<p><input type="text" class="Inputfield2" name="title" value="<?php echo isset($_POST['title']) ? htmlspecialchars($_POST['title']) : ''; ?>" /> title</p>
-	<p><input type="text" class="Inputfield2" name="author" value="<?php echo isset($_POST['author']) ? htmlspecialchars($_POST['author']) : ''; ?>" /> author</p>
-    <p><input type="text" class="Inputfield1" name="row" value="<?php echo isset($_POST['row']) ? htmlspecialchars($_POST['row']) : ''; ?>" /> row (top row is 1)</p>
-    <p><input type="text" class="Inputfield1" name="position" value="<?php echo isset($_POST['position']) ? htmlspecialchars($_POST['position']) : ''; ?>" /> 
-        position (cm from left border)</p>
-    <p>
-        <input type="submit" name="button1" class="Button" value="create book"/>
-    </p>
-</form>
-
-
-<?php
-if (isset($_REQUEST["button1"])){ // if button1 was clicked on: create
-    if(($_POST['title']!='') & ($_POST['author']!='') & ($_POST['row']!='') & ($_POST['position']!='')){
-        $bookID = create_book($_POST['title'],$_POST['author'],$_POST['row'],$_POST['position']);
-        if(is_numeric($bookID)){
-            $message = "book created with ID $bookID";
-        }else{
-            echo "creation failed";
-        }        
-    }else{
-        echo "input parameter missing";
-    }
-}
-?>
-
-<hr />
-<h3>Delete books from DB</h3>
-
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <p>
-        <select name="books1" class="DropDown" size="1">
-            <?php book_select(isset($_POST['books1']) ? substr($_POST['books1'], 2) : 0); ?>
-        </select> select title
-    </p>
-    <p><input type="submit" name="button2" class="Button" value="delete"/></p>
-</form>
-
-<?php
-if (isset($_REQUEST["button2"])){ // if button2 was clicked on: create
-    $bookID = substr($_POST['books1'], 2);
-    delete_book($bookID);
-    $message = "book deleted";
-}
-?>
-
-<hr />
-<h3>Set location parameter of books</h3>
-
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <p>
-        <select name="books2" class="DropDown2" size="1">
-            <?php book_select(isset($_POST['books2']) ? substr($_POST['books2'], 2) : 0); ?>
-        </select> select title
-    </p>
-    <p><input type="text" class="Inputfield1" name="row2" value="<?php echo isset($_POST['row2']) ? htmlspecialchars($_POST['row2']) : ''; ?>" /> row (top row is 1)</p>
-    <p><input type="text" class="Inputfield1" name="position2" value="<?php echo isset($_POST['position2']) ? htmlspecialchars($_POST['position2']) : ''; ?>" /> 
-        position (cm from left border)</p>
-    <p><input type="submit" name="button3" class="Button" value="set location"/></p>
-</form>
-
-<?php
-if (isset($_REQUEST["button3"])){ // if button3 was clicked on: create
-    if(($_POST['row2']!='') & ($_POST['position2']!='')){
-        $bookID = substr($_POST['books2'], 2);
-        set_location($bookID, $_POST['row2'], $_POST['position2']);
-        $message = "location parameter set";
-    }else{
-        echo "input parameter missing";
-    }
-}
-
-?>
-<hr />
-<div class="Message">
-<p><?php echo $message; ?></p>
-</div>
-
-</body>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <link rel="stylesheet" media="all" href="css/style_screen.css" />
+    <link rel="stylesheet" media="only screen and (max-device-width:480px)" href="css/style_handheld.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>
+      Library Management System
+    </title>
+  </head>
+  <body>
+    <div class="layoutPage">
+      <div class="layoutPageHead">
+        <div class="globalLogo left">
+          Library Management System
+        </div>
+      </div>
+      <div class="globalNavigationMain">
+        <ul>
+          <li>
+            <a href="index.php">Find Book</a>
+          </li>
+          <li>
+            <a href="register_books.php">Detect Book</a>
+          </li>
+          <li>
+            <a href="add_book.php">Add Book</a>
+          </li>
+          <li class="active">
+            <a href="books.php">Manage Books</a>
+          </li>
+        </ul>
+      </div>
+      <div class="layoutPageBody">
+        <h3>
+          Get and Set Location of Books
+        </h3>
+        <?php
+                if (isset($_POST['row2'])) {
+                    $row = $_POST['row2'];
+                    $dist = $_POST['dist2'];
+                    if (isset($_REQUEST["button3"])){ // if button3 was clicked on
+                        $bookID = substr($_POST['books2'], 2);
+                        $pos = get_location($bookID);
+                        $row = $pos['row'];
+                        $dist = $pos['dist'];
+                    }    
+                    if (isset($_REQUEST["button4"])){ // if button4 was clicked on
+                        if(($_POST['row2']!='') & ($_POST['dist2']!='')){
+                            $bookID = substr($_POST['books2'], 2);
+                            set_location($bookID, $_POST['row2'], $_POST['dist2']);
+                            $message = "location parameter set";
+                        }else{
+                            $message =  "input parameter missing";
+                        }
+                    }
+                }
+            ?>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+          <p>
+            <select name="books2" class="DropDown" size="1">
+              <?php book_select(isset($_POST['books2']) ? substr($_POST['books2'], 2) : 0); ?>
+            </select> select title
+          </p>
+          <p>
+            <input type="text" class="Inputfield1" name="row2" value="<?php echo isset($_POST['row2']) ? htmlspecialchars($row) : ''; ?>" /> row (top row is 1)
+          </p>
+          <p>
+            <input type="text" class="Inputfield1" name="dist2" value="<?php echo isset($_POST['dist2']) ? htmlspecialchars($dist) : ''; ?>" /> position (cm from left border)
+          </p>
+          <p>
+            <input type="submit" name="button3" class="Button" value="get location" /><input type="submit" name="button4" class="Button" value="set location" />
+          </p>
+        </form>
+        <hr />
+        <h3>
+          Delete books from DB
+        </h3>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+          <p>
+            <select name="books1" class="DropDown" size="1">
+              <?php book_select(isset($_POST['books1']) ? substr($_POST['books1'], 2) : 0); ?>
+            </select> select title
+          </p>
+          <p>
+            <input type="submit" name="button2" class="Button" value="delete" />
+          </p>
+        </form><?php
+            if (isset($_REQUEST["button2"])){ // if button2 was clicked on: create
+                $bookID = substr($_POST['books1'], 2);
+                delete_book($bookID);
+                $message = "book deleted";
+            }
+            ?>
+        <div class="Message">
+          <p>
+            <?php echo $message; ?>
+          </p>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
--- index.php
+++ index.php
@@ -1,50 +1,75 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php
-// printing out php errors for debugging
-error_reporting (E_ALL | E_STRICT);
-ini_set ('display_errors' , 1);
+  // printing out php errors for debugging
+  error_reporting (E_ALL | E_STRICT);
+  ini_set ('display_errors' , 1);
 
-include 'modules/m_books.php';
-?>
-
-<html>
-<head>
-<link rel="stylesheet" media="all" href="css/style_screen.css">
-<link rel="stylesheet" media="only screen and (max-device-width:480px)" href="css/style_handheld.css">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Library</title>
-</head>
-
-<body>
-
-<h1>Library</h1>
-
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <p>
-        <select name="books" class="DropDown" size="1">
-            <?php book_select(isset($_POST['books']) ? substr($_POST['books'], 2) : 0); ?>
-        </select>
-    </p>
-    <p>
-        <input type="submit" name="button1" class="Button" value="find"/>
-        <input type="submit" name="button2" class="Button" value="stop"/>
-    </p>
-</form>
-
-<?php
-if (isset($_REQUEST["button1"])){ // if button1 was clicked on: find book
-    $bookID = substr($_POST['books'], 2); // convert IDxxx -> xxx
-    $ret = calculate_xy($bookID);
-    $command = "sudo python scripts/laser_xy.py -x " . $ret['x'] . " -y " . $ret['y'];
-    //echo $command;
-    exec($command);
-}
-if (isset($_REQUEST["button2"])){ // if button2 was clicked on: stop laser
-    $command = "sudo python scripts/laser_stop.py";
-    exec($command);
-}
-?>
-
-</body>
+  include 'modules/m_books.php';
+  $message = "";
+?>    
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <link rel="stylesheet" media="all" href="css/style_screen.css" />
+    <link rel="stylesheet" media="only screen and (max-device-width:480px)" href="css/style_handheld.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>
+      Library Management System
+    </title>
+  </head>
+  <body>
+    <div class="layoutPage">
+      <div class="layoutPageHead">
+        <div class="globalLogo left">
+            Library Management System
+        </div>
+      </div>
+      <div class="globalNavigationMain">
+        <ul>
+          <li class="active">
+            <a href="index.php">Find Book</a>
+          </li>
+          <li>
+            <a href="register_books.php">Detect Book</a>
+          </li>
+          <li>
+            <a href="add_book.php">Add Book</a>
+          </li>
+          <li>
+            <a href="books.php">Manage Books</a>
+          </li>
+        </ul>
+      </div>
+      <div class="layoutPageBody">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+          <p>
+            <select name="books" class="DropDown" size="1">
+                <?php 
+                    if(isset($_POST['books'])){
+                        $bookID = intval(substr($_POST['books'], 2));
+                    }else{
+                        $bookID = 0;
+                    }
+                    book_select($bookID, True); 
+                    ?>
+            </select>
+          </p>
+          <p>
+            <input type="submit" name="button1" class="Button" value="find book" /> <input type="submit" name="button2" class="Button" value="laser off" />
+          </p>
+        </form><?php
+                    if (isset($_REQUEST["button1"])){ // if button1 was clicked on: find book
+                        $bookID = substr($_POST['books'], 2); // convert IDxxx -> xxx
+                        $message = laser_to_book($bookID);
+                    }
+                    if (isset($_REQUEST["button2"])){ // if button2 was clicked on: stop laser
+                        set_parameter("Action", "Stop"); // this parameter is then read by the laser_daemon process
+                    }
+                    ?>
+        <div class="Message">
+            <p><?php echo $message; ?></p>
+        </div>                    
+      </div>
+    </div>
+  </body>
 </html>
-
